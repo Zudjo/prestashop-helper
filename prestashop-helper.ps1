a410155a9884584526e3e9af91afb9f9918659ae
Zudjo <giorgio.ziu.alo@gmail.com>
2023-05-05 15:04:57 +0200

    Fixed some issues

--- prestashop-helper.ps1
+++ prestashop-helper.ps1
@@ -1,4 +1,5 @@
 $XamppPath = "C:\xampp\htdocs\"
+$CurrentPath = $PWD.Path
 
 Remove-Item -Path $XamppPath -Recurse -Force
 
@@ -6,8 +7,12 @@ git clone https://github.com/PrestaShop/PrestaShop.git "C:\xampp\htdocs"
 
 .\set-permissions.ps1
 
+Set-Location $XamppPath
+
 composer install
 
 npm install -g win-node-env
+
+Set-Location $CurrentPath
 
 .\first-compilation.ps1
